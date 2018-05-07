84dfa947289d59d720118a8bb9f16e1bd31e0274
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-05-07 08:32:41 +0200

    Working on pretty UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ const Card = ({my_card, choosen_card, onChoosenClick, onClick}) => {
     <div
       className="my-card"
       onClick={(choosen_card===1)?onChoosenClick:onClick}
-      style={{animationName:translateFrames, animationDuration:'0.5s', left:'20px', top:(20-140*choosen_card)+'px'}}>
+      style={{animationName:translateFrames, animationDuration:'0.5s', left:'20px', top:(20-110*choosen_card)+'px'}}>
         <img className='my-card-bg-image' src='./images/card-base.png' alt=""></img>
         <img className='my-card-corner-image' src={my_card.corner_image} alt=""></img>
         <div className='my-card-header'>{my_card.header}</div>
