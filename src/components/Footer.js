9d93607e30813a4021dc9429c7e75dd1f2d01537
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-05-07 08:04:35 +0200

    footer display fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,9 @@ const Footer = ({visible, war_points, happy_points, burnt_points, my_role}) => (
     <div className='my-footer'>
       <div className="my-footer-bg align-items-center p-2 px-md-2">
         <div className="row">
-          <div className="col-3 text-center"><span className="h1">{war_points}</span><img alt="" src="images/score-icon2.png" /></div>
-          <div className="col-3 text-center"><span className="h1">{happy_points}</span><img alt="" src="images/score-icon3.png" /></div>
-          <div className="col-3 text-center"><span className="h1">{burnt_points}</span><img alt="" src="images/score-icon1.png" /></div>
-          <div className="col-3 text-center"></div>
+          <div className="col-4 text-center"><span className="h1">{war_points}</span><img alt="" src="images/score-icon2.png" /></div>
+          <div className="col-4 text-center"><span className="h1">{happy_points}</span><img alt="" src="images/score-icon3.png" /></div>
+          <div className="col-4 text-center"><span className="h1">{burnt_points}</span><img alt="" src="images/score-icon1.png" /></div>
         </div>
       </div>
     </div>}
