26c3ca6a1997d861626b80907a4575b587e4b41b
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-05-08 16:51:46 +0200

    Adding scnario info

--- src/connectors/ChooseScenario.js
+++ src/connectors/ChooseScenario.js
@@ -1,48 +1,17 @@
 import { connect } from 'react-redux'
 import List from '../components/List'
 import { chooseScenario } from '../actions'
+import { SCENARIO_A1, SCENARIO_A2, SCENARIO_A3, SCENARIO_A4 } from '../include/scenarioDefinitions'
 //import debug from '../include/debug'
 
 const mapStateToProps = (state) => ({
   visible: (state.mymap.scenario_choosen===0),
   header: 'Wybierzcie scenariusz',
-  items: [{
-    id: 'scenario-a1',
-    title: 'A1. Silny król i chłopi',
-    subtitle:[
-      'Dla 1-8 graczy: Król, 2x Rycerz, 5x Chłop',
-      'Scenariusz do poznania gry i integracji grupy.',
-      'Dodatkowe zasady:',
-      '- Każdy musi zdobyć minimum 5 punktów wojny',
-      '- Czas gry jest ograniczony do 15 minut']},
-    {
-    id: 'scenario-a2',
-    title: 'A2. Ciche królestwo',
-    subtitle:[
-      'Dla 1-5 graczy: Król, Buntownik, Poborca, 2x Rycerz',
-      'Scenariusz do symulacji problemów w komunikacji',
-      'Dodatkowe zasady:',
-      '- Każdy musi zdobyć minimum 2 punkty szczęścia',
-      '- W czasie gry nie wolno mówić']},
-    {
-    id: 'scenario-a3',
-    title: 'A3. Ślepiec',
-    subtitle:[
-      'Dla 1-4 graczy. W opracowaniu',
-      'Scenariusz do niskiej wiedzy lidera i zespołu',
-      'Dodatkowe zasady:',
-      '- Każdy musi zdobyć minimum 5 punktów',
-      '- Czas gry jest ograniczony do 10 minut']},
-    {
-    id: 'scenario-a4',
-    title: 'A4. Młody król wśród wilków',
-    subtitle:[
-      'Dla 4-12 graczy. W opracowaniu',
-      'Scenariusz do symulacji pracy w warunkach ciężkiego konfliktu i sprzecznych interesów.',
-      'Dodatkowe zasady:',
-      '- Każdy musi zdobyć minimum 1 punkt',
-      '- Czas gry jest ograniczony do 10 minut',
-      '- Wolno rozmawiać tylko po angielsku']}
+  items: [
+    SCENARIO_A1,
+    SCENARIO_A2,
+    SCENARIO_A3,
+    SCENARIO_A4
   ]
 })
 
---
+++ src/include/scenarioDefinitions.js
@@ -0,0 +1,44 @@
+export const SCENARIO_A1 = {
+  id: 'scenario-a1',
+  title: 'A1. Silny król i chłopi',
+  subtitle:[
+    'Dla 1-8 graczy: Król, 2x Rycerz, 5x Chłop',
+    'Scenariusz do poznania gry i integracji grupy.',
+    'Dodatkowe zasady:',
+    '- Każdy musi zdobyć minimum 5 punktów wojny',
+    '- Czas gry jest ograniczony do 15 minut'
+  ]}
+
+export const SCENARIO_A2 = {
+  id: 'scenario-a2',
+  title: 'A2. Ciche królestwo',
+  subtitle:[
+    'Dla 1-5 graczy: Król, Buntownik, Poborca, 2x Rycerz',
+    'Scenariusz do symulacji problemów w komunikacji',
+    'Dodatkowe zasady:',
+    '- Każdy musi zdobyć minimum 2 punkty szczęścia',
+    '- W czasie gry nie wolno mówić'
+  ]}
+
+export const SCENARIO_A3 = {
+  id: 'scenario-a3',
+  title: 'A3. Ślepiec',
+  subtitle:[
+    'Dla 1-4 graczy. W opracowaniu',
+    'Scenariusz do niskiej wiedzy lidera i zespołu',
+    'Dodatkowe zasady:',
+    '- Każdy musi zdobyć minimum 5 punktów',
+    '- Czas gry jest ograniczony do 10 minut'
+  ]}
+
+export const SCENARIO_A4 = {
+  id: 'scenario-a4',
+  title: 'A4. Młody król wśród wilków',
+  subtitle:[
+    'Dla 4-12 graczy. W opracowaniu',
+    'Scenariusz do symulacji pracy w warunkach ciężkiego konfliktu i sprzecznych interesów.',
+    'Dodatkowe zasady:',
+    '- Każdy musi zdobyć minimum 1 punkt',
+    '- Czas gry jest ograniczony do 10 minut',
+    '- Wolno rozmawiać tylko po angielsku'
+  ]}
