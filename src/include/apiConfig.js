5a41e3d0eda491700b57d49cea515b68b3894233
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-07-13 16:54:19 +0200

    Fix for AWS

--- src/include/apiConfig.js
+++ src/include/apiConfig.js
@@ -1,5 +1,5 @@
 import getUrlParam from '../include/getUrlParam'
 
-export const API_URL='http://35.158.92.99';
+export const API_URL='http://reconquista.ignifer-labs.com';
 export const GAME_ID = getUrlParam('game_id');
 export const USER_ID = getUrlParam('user_id');
